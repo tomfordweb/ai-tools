980749f3334c8150544d8bf1fa9a1e98bc8a3e2d
Tom Ford <tomfordweb@gmail.com>
2025-11-30 13:17:38 -0500

    more models

--- bin/install.sh
+++ bin/install.sh
@@ -6,7 +6,10 @@
 ./bin/downloadModel.sh "gemma3:270m" 
 ./bin/downloadModel.sh "gemma3:1b"
 ./bin/downloadModel.sh "codellama"
+./bin/downloadModel.sh "deepseek-r1:1.5b"
+./bin/downloadModel.sh "phi3:3.8b"
+./bin/downloadModel.sh "qwen3:1.7b"
+./bin/downloadModel.sh "qwen3:0.6b"
+./bin/downloadModel.sh "tinyllama"
 
-# cloud
-./bin/downloadModel.sh "glm-4.6"
 
